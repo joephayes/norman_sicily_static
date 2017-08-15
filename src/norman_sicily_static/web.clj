207a5d3bba53cfc76cd041053bf27239597f7fdd
Joe Hayes <joephayes@gmail.com>
2017-08-14 22:36:18 -0400

    use https for jquery CDN link

--- src/norman_sicily_static/web.clj
+++ src/norman_sicily_static/web.clj
@@ -36,7 +36,7 @@
       page]
       util/footer
      [:script
-      {:src "http://code.jquery.com/jquery-3.2.1.slim.min.js"
+      {:src "https://code.jquery.com/jquery-3.2.1.slim.min.js"
        :integrity "sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
        :crossorigin "anonymous"}]
      (include-js (link/file-path request "/scripts/bootstrap.min.js"))
