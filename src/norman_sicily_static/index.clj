09de3594489d085239a18199b8c5ff686991c7e0
Joe Hayes <joephayes@gmail.com>
2017-09-11 01:57:15 -0400

    update embedded youtube video id

--- src/norman_sicily_static/index.clj
+++ src/norman_sicily_static/index.clj
@@ -6,4 +6,4 @@
    [:div {:class "row"}
     [:div {:class "col-lg-12"}
      [:div {:class "center-block", :style "width:712px;height:400px;"}
-      [:iframe {:style "overflow:hidden;height:100%;width:100%", :width "100%", :height "100%", :src "http://www.youtube.com/embed/65v2GR6SCd0?rel=0&modestbranding=1&showinfo=0", :frameborder "0", :allowfullscreen true}]]]]])
+      [:iframe {:style "overflow:hidden;height:100%;width:100%", :width "100%", :height "100%", :src "http://www.youtube.com/embed/0brgzGyg59s?rel=0&modestbranding=1&showinfo=0", :frameborder "0", :allowfullscreen true}]]]]])
