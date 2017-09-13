28f539831849bef9577274ecdd5504481af56175
Joe Hayes <joephayes@gmail.com>
2017-09-13 00:38:41 -0400

    make background of roger's cloak transparent (chattels page)

--- src/norman_sicily_static/chattels.clj
+++ src/norman_sicily_static/chattels.clj
@@ -11,7 +11,7 @@
        [:p "This page will ultimately offer links to various categories of moveable objects produced in the Norman Kingdom of Sicily."]
        [:figure
         [:p]
-        [:img {:class "img-responsive centered-responsive-img", :src "/images/sicilo-moorishcloak-990X459.jpg", :alt "Roger II's Coronation Mantle"}]
+        [:img {:class "img-responsive centered-responsive-img", :src "/images/sicilo-moorishcloak-990X459.png", :alt "Roger II's Coronation Mantle"}]
         [:figcaption "Roger II&#39;s Coronation Mantle, Palermo, c. 1133"
          [:br]"(Gold Embroidery on Red Samite with Pearls and Precious Stones)"
          [:br]"Treasury, "
