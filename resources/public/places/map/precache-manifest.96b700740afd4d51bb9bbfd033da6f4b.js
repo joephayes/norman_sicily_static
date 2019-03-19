ddb9ef969fbae0bef921c081bfb60c768366b736
Joe Hayes <joephayes@gmail.com>
2019-03-19 00:17:59 -0400

    interactive map look and feel updates

--- resources/public/places/map/precache-manifest.96b700740afd4d51bb9bbfd033da6f4b.js
+++ resources/public/places/map/precache-manifest.96b700740afd4d51bb9bbfd033da6f4b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "2e9cc4810c2748e003b9",
-    "url": "/places/map/static/css/main.4719122f.chunk.css"
+    "revision": "8a41a1a23cdde1697b45",
+    "url": "/places/map/static/css/main.fc6f8b00.chunk.css"
   },
   {
-    "revision": "2e9cc4810c2748e003b9",
-    "url": "/places/map/static/js/main.bab6873f.chunk.js"
+    "revision": "8a41a1a23cdde1697b45",
+    "url": "/places/map/static/js/main.86fe368f.chunk.js"
   },
   {
     "revision": "ee42fe93f25666d021b4",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/places/map/static/js/2.cb62db10.chunk.js"
   },
   {
-    "revision": "bb1fcea18d786e59ca57283688f4cbe6",
+    "revision": "40ac77d18dc8b21bbb45040ddb5dd0f1",
     "url": "/places/map/index.html"
   }
 ];
--- src/norman_sicily_static/places.clj
+++ src/norman_sicily_static/places.clj
@@ -7,6 +7,5 @@
     [:div {:class "col-lg-12"}
      [:div {:class "inner"}
       [:div {:class "text-center"}
-       [:div {:class "credits-header"} "Places Page"]
        [:iframe {:src "/places/map/"
-                 :style "width:100%;height:100%;min-height:620px;min-width:1000px"}]]]]]])
+                 :style "border:0px;width:100%;height:100%;min-height:620px;min-width:1000px"}]]]]]])
