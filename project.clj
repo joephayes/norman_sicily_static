3a9691df0f6ce6d3eed3c8a3e20f7701d4787409
Joe Hayes <joephayes@gmail.com>
2019-11-18 18:12:00 -0500

    Integrate NSP Genealogy work into site
    Squashed commit of the following:
    
    commit ddd298526db612e94761a3680ad5fbec8b32b196
    Author: Joe Hayes <joephayes@gmail.com>
    Date:   Mon Nov 18 18:10:08 2019 -0500
    
        Add note about mother child link of Elvira and Gisela
    
    commit ad36cdfc23c0556980e988ad7daf6a6726ad5fbb
    Author: Joe Hayes <joephayes@gmail.com>
    Date:   Mon Nov 18 00:53:37 2019 -0500
    
        people page - hauteville to nsp
    
    commit 3877adc81e9fc38bedad4a7506af07649efb1276
    Author: Joe Hayes <joephayes@gmail.com>
    Date:   Fri Nov 15 01:37:37 2019 -0500
    
        color palette
    
    commit b9c54cc1305b5887288c915c1a510d244dc4ab86
    Author: Joe Hayes <joephayes@gmail.com>
    Date:   Fri Nov 15 01:08:43 2019 -0500
    
        color palette, link and node size on hover
    
    commit e51661d4b1caa6664e3f5a8b3fecdfa13c07c547
    Author: Joe Hayes <joephayes@gmail.com>
    Date:   Thu Nov 14 09:17:54 2019 -0500
    
        Get rid of separate labels array
    
    commit d9c087f1b54bb036009bca7cd388112b3df45ef5
    Author: Joe Hayes <joephayes@gmail.com>
    Date:   Wed Nov 13 23:19:43 2019 -0500
    
        integrate hauteville geneaology tree into static site
        dependency updates
        fix styling of iframes (make them responsive)

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://www.normansicily.org"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [stasis "2.4.0"]
-                 [ring "1.7.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [stasis "2.5.0"]
+                 [ring "1.8.0"]
                  [hiccup "1.0.5"]
                  [enlive "1.1.6"]
                  [optimus "0.20.2"]
                  [optimus-less "0.2.1"]
-                 [cheshire "5.8.1"]]
+                 [cheshire "5.9.0"]]
   :plugins [[lein-ftp-static-deploy "0.1.0"]]
   :ring {:handler norman-sicily-static.web/app
          :auto-refresh? true}
@@ -19,7 +19,7 @@
             "serve" ["ring" "server"]}
   :clean-targets [:target-path "dist"]
   :profiles {:dev {:plugins [[lein-ring "0.12.5"]]}
-             :test {:dependencies [[midje "1.9.6"]]
+             :test {:dependencies [[midje "1.9.9"]]
                     :plugins [[lein-midje "3.2.1"]]}}
   :ftp {:host ~(System/getenv "FTP_DEPLOY_HOST")
         :user ~(System/getenv "FTP_DEPLOY_USER")
