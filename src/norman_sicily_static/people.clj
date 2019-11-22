70eff18f7157e703b27816f0e281247ff7895dac
Joe Hayes <joephayes@gmail.com>
2019-11-21 23:45:00 -0500

    look and feel (#3)
    * Look and feel changes in map and people page

--- src/norman_sicily_static/people.clj
+++ src/norman_sicily_static/people.clj
@@ -10,4 +10,4 @@
        [:iframe {:src "/people/family-tree/nsp.html"}]]
       [:div {:class "text-center top-padding"}
        [:p "Hover over individual nodes to discover relationships. Nodes can be moved with a click of the mouse and you may zoom in or zoom out on the graph."]
-       [:p "The mother-child link between Elvira of León-Castile and Gisela of Burgundy is based on Dr. Hayes' proposal in " [:em "Roger II of Sicily: Family, Faith and Empire in the Medieval Mediterranean World (Brepols, 2020)"]]]]]]])
+       [:p "The mother-child link between Elvira of León-Castile and Gisela of Burgundy is based on Dr. Hayes' proposal in " [:em "Roger II of Sicily: Family, Faith and Empire in the Medieval Mediterranean World "] " (Brepols, 2020)."]]]]]])
